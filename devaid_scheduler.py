0d9d5516ff9fc9df3ed04fd7dcffaa350522f0b1
laurianeteyssier <lteyssier@laterite.com>
2025-10-16 13:02:35 +0200

    change scheduler to production rythm

--- devaid_scheduler.py
+++ devaid_scheduler.py
@@ -11,8 +11,8 @@ def job():
     devaid.fetch_multiple_tenders_details(new_tender_ids[:5])
 
 
-schedule.every(5).minutes.do(job)  # for testing
-# schedule.every().tuesday.at("09:00").do(job)  # for production
+# schedule.every(5).minutes.do(job)  # for testing
+schedule.every().tuesday.at("09:00").do(job)  # for production
 
 while True:
     schedule.run_pending()
