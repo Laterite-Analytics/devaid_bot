72adc092a8908518ff930026de73d87f27503b99
laurianeteyssier <lteyssier@laterite.com>
2025-10-16 11:14:40 +0200

    update cron for test (every 5 minutes)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "Starting weekly tender fetcher cron..."
-echo "0 9 * * TUE python /app/devaid.py" > /etc/crontab
+#echo "0 9 * * TUE python /app/devaid.py" > /etc/crontab
+echo "*/5 * * * * root python /app/devaid.py" > /etc/crontab
 cron -f
