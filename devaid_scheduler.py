7f10d0fc691504db07025f9276985bd1cb494246
laurianeteyssier <lteyssier@laterite.com>
2025-10-24 13:55:49 +0200

    update slack version, add logging within the container

--- devaid_scheduler.py
+++ devaid_scheduler.py
@@ -1,20 +1,24 @@
+import logging
 import time
 
 import schedule
 
 import devaid
 
+logging.basicConfig(level=logging.INFO)
+
 
 def job():
-    print("Running scheduled job...")
+    logging.info("Running scheduled job...")
     new_tender_ids = devaid.fetch_new_tenders()
     devaid.fetch_multiple_tenders_details(new_tender_ids[:5])
 
 
-# schedule.every(5).minutes.do(job)  # for testing
-for day in ["monday", "tuesday", "wednesday", "thursday", "friday"]:
-    getattr(schedule.every(), day).at("09:00").do(job)
+schedule.every(5).minutes.do(job)  # for testing
+# for day in ["monday", "tuesday", "wednesday", "thursday", "friday"]:
+#     getattr(schedule.every(), day).at("09:00").do(job)
 
 while True:
     schedule.run_pending()
+    logging.info("Schedule launched")
     time.sleep(120)
